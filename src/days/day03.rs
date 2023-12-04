9deb775e75ccb691eab0fd9335a2438187b3ec53
drewtato <15526875+drewtato@users.noreply.github.com>
2023-12-03 17:24:46 -0800

    More neighbor things (my favorite)

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -7,39 +7,42 @@ pub type A2 = impl Display + Debug + Clone;
 
 #[derive(Debug, Default, Clone)]
 pub struct Solution {
-	file: Grid<(u8, Cell<(u8, u32)>)>,
+	file: Grid<Item>,
 }
+
+#[derive(Debug, Default, Clone)]
+struct Item(u8, Cell<(u8, u32)>);
 
 impl Solver for Solution {
 	type AnswerOne = A1;
 	type AnswerTwo = A2;
 
 	fn initialize(file: Vec<u8>, _: u8) -> Self {
-		let file = file.grid(|c| (c, (0, 1).into()));
+		let file = file.grid(|c| Item(c, (0, 1).into()));
 		Self { file }
 	}
 
 	fn part_one(&mut self, _: u8) -> Self::AnswerOne {
 		let mut total = 0;
 		for (y, row) in self.file.iter().enumerate() {
-			for (x, &(item, _)) in row.iter().enumerate() {
+			for (x, &Item(item, _)) in row.iter().enumerate() {
 				if item.is_ascii_digit()
 					&& !self.file[y]
 						.get(x.wrapping_sub(1))
-						.is_some_and(|(c, _)| c.is_ascii_digit())
+						.is_some_and(|Item(c, _)| c.is_ascii_digit())
 				{
 					let mut number = 0;
 					let mut near_symbol = false;
 					for ox in 0.. {
 						let nx = ox + x;
-						if let Some(&(c, _)) = self.file[y].get(nx) {
+						if let Some(&Item(c, _)) = self.file[y].get(nx) {
 							if c.is_ascii_digit() {
 								number *= 10;
 								number += (c as char).to_digit(10).unwrap();
 								if self
 									.file
 									.neighbors_iter(y, nx)
-									.any(|&(c, _)| !c.is_ascii_digit() && c != b'.')
+									.any(|&Item(c, _)| !c.is_ascii_digit() && c != b'.')
 								{
 									near_symbol = true;
 								}
@@ -62,25 +65,23 @@ impl Solver for Solution {
 	fn part_two(&mut self, _: u8) -> Self::AnswerTwo {
 		let mut total = 0;
 		for (y, row) in self.file.iter().enumerate() {
-			for (x, &(item, _)) in row.iter().enumerate() {
+			for (x, &Item(item, _)) in row.iter().enumerate() {
 				if item.is_ascii_digit()
 					&& !self.file[y]
 						.get(x.wrapping_sub(1))
-						.is_some_and(|(c, _)| c.is_ascii_digit())
+						.is_some_and(|Item(c, _)| c.is_ascii_digit())
 				{
 					let mut number = 0;
 					let mut near_symbol = None;
 					for ox in 0.. {
 						let nx = ox + x;
-						if let Some(&(c, _)) = self.file[y].get(nx) {
+						if let Some(&Item(c, _)) = self.file[y].get(nx) {
 							if c.is_ascii_digit() {
 								number *= 10;
-								number += (c as char).to_digit(10).unwrap();
-								for (oy, row) in self.file.neighbors(y, nx).iter().enumerate() {
-									for (ox, &&(c, _)) in row.iter().flatten().enumerate() {
-										if c == b'*' {
-											near_symbol = Some((y + oy - 1, nx + ox - 1));
-										}
+								number += (c - b'0') as u32;
+								for (&Item(c, _), cy, cx) in self.file.neighbors_coords(y, nx) {
+									if c == b'*' {
+										near_symbol = Some((cy, cx));
 									}
 								}
 							} else {
--- src/helpers/neighbors.rs
+++ src/helpers/neighbors.rs
@@ -11,10 +11,10 @@ pub type NeighborsExtra<'a, const Y: usize, const X: usize, T> = [[Option<&'a T>
 
 /// Collection type that can return neighbors of a `(y, x)` location.
 pub trait GetNeighbors {
-	/// The collection's item type
+	/// The collection's item type.
 	type Neighbor;
 
-	/// The 3x3 neighbors centered on the selected coordinate
+	/// The 3x3 neighbors centered on the selected coordinate.
 	fn neighbors(&self, y: usize, x: usize) -> Neighbors<Self::Neighbor> {
 		self.neighbors_extra(y, x)
 	}
@@ -39,12 +39,12 @@ pub trait GetNeighbors {
 		off_x: isize,
 	) -> NeighborsExtra<Y_LEN, X_LEN, Self::Neighbor>;
 
-	/// Iterator over the 3x3 neighbors of an element, including itself
+	/// Iterator over the 3x3 neighbors of an element, including itself.
 	fn neighbors_iter(&self, y: usize, x: usize) -> NeighborIter<Self::Neighbor, 3, 3> {
 		self.neighbors(y, x).into_iter().flatten().flatten()
 	}
 
-	/// Iterator over the YxX neighbors of an element, including itself
+	/// Iterator over the YxX neighbors of an element, including itself.
 	fn neighbors_extra_iter<const Y_LEN: usize, const X_LEN: usize>(
 		&self,
 		y: usize,
@@ -69,10 +69,98 @@ pub trait GetNeighbors {
 			.flatten()
 			.flatten()
 	}
+
+	/// Iterator over all the elements from [`GetNeighbors::neighbors_extra_offset`] with
+	/// coordinates.
+	fn neighbors_extra_offset_coords<const Y_LEN: usize, const X_LEN: usize>(
+		&self,
+		y: usize,
+		x: usize,
+		off_y: isize,
+		off_x: isize,
+	) -> NeighborCoords<Self::Neighbor, Y_LEN, X_LEN> {
+		NeighborCoords::new(
+			self.neighbors_extra_offset::<Y_LEN, X_LEN>(y, x, off_y, off_x),
+			y.wrapping_add_signed(off_y),
+			x.wrapping_add_signed(off_x),
+		)
+	}
+
+	/// Iterator over all the elements from [`GetNeighbors::neighbors_extra`] with
+	/// coordinates.
+	fn neighbors_extra_coords<const Y_LEN: usize, const X_LEN: usize>(
+		&self,
+		y: usize,
+		x: usize,
+	) -> NeighborCoords<Self::Neighbor, Y_LEN, X_LEN> {
+		NeighborCoords::new(
+			self.neighbors_extra::<Y_LEN, X_LEN>(y, x),
+			((Y_LEN as isize - 1) / -2) as usize,
+			((X_LEN as isize - 1) / -2) as usize,
+		)
+	}
+
+	/// Iterator over all the elements from [`GetNeighbors::neighbors`] with
+	/// coordinates.
+	fn neighbors_coords(&self, y: usize, x: usize) -> NeighborCoords<Self::Neighbor, 3, 3> {
+		NeighborCoords::new(
+			self.neighbors_extra::<3, 3>(y, x),
+			y.wrapping_sub(1),
+			x.wrapping_sub(1),
+		)
+	}
 }
 
 type NeighborIter<'a, T, const Y: usize, const X: usize> =
 	Flatten<Flatten<std::array::IntoIter<[Option<&'a T>; X], Y>>>;
+
+#[derive(Debug, Clone, Copy)]
+pub struct NeighborCoords<'a, T, const Y_LEN: usize, const X_LEN: usize> {
+	neighbors: [[Option<&'a T>; X_LEN]; Y_LEN],
+	index: usize,
+	offset_y: usize,
+	offset_x: usize,
+}
+
+impl<'a, T, const Y_LEN: usize, const X_LEN: usize> Iterator
+	for NeighborCoords<'a, T, Y_LEN, X_LEN>
+{
+	type Item = (&'a T, usize, usize);
+
+	fn next(&mut self) -> Option<Self::Item> {
+		loop {
+			let y = self.index / X_LEN;
+			let x = self.index % X_LEN;
+			if let Some(&opt) = self.neighbors.get(y).and_then(|row| row.get(x)) {
+				self.index += 1;
+				if let Some(item) = opt {
+					break Some((
+						item,
+						y.wrapping_add(self.offset_y),
+						x.wrapping_add(self.offset_x),
+					));
+				}
+			} else {
+				break None;
+			}
+		}
+	}
+}
+
+impl<'a, T, const Y_LEN: usize, const X_LEN: usize> NeighborCoords<'a, T, Y_LEN, X_LEN> {
+	fn new(neighbors: [[Option<&'a T>; X_LEN]; Y_LEN], offset_y: usize, offset_x: usize) -> Self {
+		Self {
+			neighbors,
+			index: 0,
+			offset_y,
+			offset_x,
+		}
+	}
+
+	pub fn reset(&mut self) {
+		self.index = 0;
+	}
+}
 
 impl<T> GetNeighbors for Grid<T> {
 	type Neighbor = T;
@@ -86,10 +174,11 @@ impl<T> GetNeighbors for Grid<T> {
 	) -> NeighborsExtra<Y_LEN, X_LEN, Self::Neighbor> {
 		array::from_fn(|dy| {
 			let ny = y + dy;
-			let ny = (ny as isize + off_y) as usize;
+			// let ny = (ny as isize + off_y) as usize;
+			let ny = ny.wrapping_add_signed(off_y);
 			array::from_fn(|dx| {
 				let nx = x + dx;
-				let nx = (nx as isize + off_x) as usize;
+				let nx = nx.wrapping_add_signed(off_x);
 				self.get(ny).and_then(|row| row.get(nx))
 			})
 		})
