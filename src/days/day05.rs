46790d97d6d969444c94b34eb299c06d911c6732
drewtato <15526875+drewtato@users.noreply.github.com>
2023-12-13 22:09:37 -0800

    Day 14 done

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,7 +1,13 @@
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::convert::identity;
+
 use crate::helpers::*;
 
-pub type A1 = impl Display + Debug + Clone;
-pub type A2 = impl Display + Debug + Clone;
+pub type A1 = u64;
+pub type A2 = A1;
 
 #[derive(Debug, Default, Clone)]
 pub struct Solution {
@@ -76,44 +82,63 @@ impl Solver for Solution {
 	}
 
 	fn part_two(&mut self, _: u8) -> Self::AnswerTwo {
-		use rayon::prelude::*;
-		self.seeds
-			// .chunks(2)
-			.par_chunks(2)
-			.flat_map(|chunk| {
-				let &[a, b] = chunk else { unreachable!() };
-				a..(a + b)
-			})
-			.map(|seed| {
-				let mut current = seed;
-				for map in &self.maps {
-					let next = match map.binary_search_by_key(&current, |[_, b, _]| *b) {
-						Ok(index) => {
-							if map[index][2] > 0 {
-								current - map[index][1] + map[index][0]
-							} else {
-								current
-							}
+		let mut ranges = self
+			.seeds
+			.array_chunks()
+			.map(|&[start, length]| [start, start + length])
+			.collect_vec();
+		let mut next_ranges = Vec::new();
+
+		for map in &self.maps {
+			println!("Ranges: {ranges:?}");
+			for &[mut start, end] in &ranges {
+				println!("Range: {:?}", [start, end]);
+				let index = map
+					.binary_search_by_key(&start, |[_, b, _]| *b)
+					.unwrap_or_else(identity)
+					.saturating_sub(1);
+
+				if index == map.len() {
+					next_ranges.push(dbg_small!([start, end]));
+				} else {
+					for line @ &[dest_start, source_start, len] in &map[index..] {
+						println!("Line: {line:?}");
+						let source_end = source_start + len;
+						let dest_end = dest_start + len;
+
+						if start >= source_end {
+							continue;
 						}
-						Err(index) => {
-							if index == 0 {
-								current
-							} else {
-								let index = index - 1;
-								if map[index][1] + map[index][2] > current {
-									current - map[index][1] + map[index][0]
-								} else {
-									current
-								}
-							}
+						if end <= source_start {
+							break;
 						}
-					};
-					current = next;
+						todo!()
+					}
 				}
-				current
-			})
-			.min()
-			.unwrap()
+			}
+
+			ranges.clear();
+			std::mem::swap(&mut ranges, &mut next_ranges);
+
+			ranges.sort_unstable();
+			next_ranges.push(ranges[0]);
+			for &[start, end] in &ranges[1..] {
+				if start == end {
+					continue;
+				}
+				let last = &mut next_ranges.last_mut().unwrap()[1];
+				if *last >= start {
+					*last = end;
+				} else {
+					next_ranges.push([start, end]);
+				}
+			}
+
+			ranges.clear();
+			std::mem::swap(&mut ranges, &mut next_ranges);
+		}
+
+		ranges.into_iter().map(|[start, _]| start).min().unwrap()
 	}
 
 	fn run_any<W: std::fmt::Write>(
--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,6 +1,4 @@
-#![allow(unused_mut)]
-#![allow(unused_variables)]
-#![allow(dead_code)]
+use std::cell::Cell;
 
 use crate::helpers::*;
 
@@ -9,23 +7,53 @@ pub type A2 = impl Display + Debug + Clone;
 
 #[derive(Debug, Default, Clone)]
 pub struct Solution {
-	file: Vec<u8>,
+	grid: Vec<Vec<Cell<u8>>>,
 }
+
+const ROUND: u8 = b'O';
+// const CUBE: u8 = b'#';
+const EMPTY: u8 = b'.';
+const CYCLES: usize = 1_000_000_000;
+const PARTIAL: usize = 1_000;
 
 impl Solver for Solution {
 	type AnswerOne = A1;
 	type AnswerTwo = A2;
 
 	fn initialize(file: Vec<u8>, _: u8) -> Self {
-		Self { file }
+		Self {
+			grid: file.grid(Cell::new),
+		}
 	}
 
 	fn part_one(&mut self, _: u8) -> Self::AnswerOne {
-		"part 1 unimplemented"
+		self.tilt_north();
+
+		// print_grid(&self.grid);
+
+		self.sum()
 	}
 
 	fn part_two(&mut self, _: u8) -> Self::AnswerTwo {
-		"part 2 unimplemented"
+		let mut cycles_iter = 0..CYCLES;
+		for _ in cycles_iter.by_ref().take(PARTIAL) {
+			self.cycle();
+		}
+
+		let mut weights = vec![self.sum()];
+		for _ in cycles_iter {
+			self.cycle();
+
+			let sum = self.sum();
+			if weights[0] == sum {
+				break;
+			} else {
+				weights.push(sum);
+			}
+		}
+		let cycle_length = weights.len();
+		let offset = (CYCLES - PARTIAL) % cycle_length;
+		weights[offset]
 	}
 
 	fn run_any<W: std::fmt::Write>(
@@ -40,3 +68,112 @@ impl Solver for Solution {
 		}
 	}
 }
+
+impl Solution {
+	fn cycle(&self) {
+		self.tilt_north();
+		self.tilt_west();
+		self.tilt_south();
+		self.tilt_east();
+	}
+	fn tilt_north(&self) {
+		for (y, row) in self.grid.iter().enumerate() {
+			for (x, cell) in row.iter().enumerate() {
+				let mut y = y as isize;
+				let x = x as isize;
+				if cell.get() == ROUND
+				// && grid_get(&self.grid, [y - 1, x]).map(|c| c.get()) == Some(EMPTY)
+				{
+					cell.set(EMPTY);
+					y -= 1;
+					while grid_get(&self.grid, [y, x]).map(|c| c.get()) == Some(EMPTY) {
+						y -= 1;
+					}
+					y += 1;
+					grid_get(&self.grid, [y, x]).unwrap().set(ROUND);
+				}
+			}
+		}
+	}
+
+	fn tilt_south(&self) {
+		for (y, row) in self.grid.iter().enumerate().rev() {
+			for (x, cell) in row.iter().enumerate() {
+				let mut y = y as isize;
+				let x = x as isize;
+				if cell.get() == ROUND
+				// && grid_get(&self.grid, [y + 1, x]).map(|c| c.get()) == Some(EMPTY)
+				{
+					cell.set(EMPTY);
+					y += 1;
+					while grid_get(&self.grid, [y, x]).map(|c| c.get()) == Some(EMPTY) {
+						y += 1;
+					}
+					y -= 1;
+					grid_get(&self.grid, [y, x]).unwrap().set(ROUND);
+				}
+			}
+		}
+	}
+
+	fn tilt_west(&self) {
+		for (y, row) in self.grid.iter().enumerate() {
+			for (x, cell) in row.iter().enumerate() {
+				let y = y as isize;
+				let mut x = x as isize;
+				if cell.get() == ROUND
+				// && grid_get(&self.grid, [y, x - 1]).map(|c| c.get()) == Some(EMPTY)
+				{
+					cell.set(EMPTY);
+					x -= 1;
+					while grid_get(&self.grid, [y, x]).map(|c| c.get()) == Some(EMPTY) {
+						x -= 1;
+					}
+					x += 1;
+					grid_get(&self.grid, [y, x]).unwrap().set(ROUND);
+				}
+			}
+		}
+	}
+
+	fn tilt_east(&self) {
+		for (y, row) in self.grid.iter().enumerate() {
+			for (x, cell) in row.iter().enumerate().rev() {
+				let y = y as isize;
+				let mut x = x as isize;
+				if cell.get() == ROUND
+				// && grid_get(&self.grid, [y, x + 1]).map(|c| c.get()) == Some(EMPTY)
+				{
+					cell.set(EMPTY);
+					x += 1;
+					while grid_get(&self.grid, [y, x]).map(|c| c.get()) == Some(EMPTY) {
+						x += 1;
+					}
+					x -= 1;
+					grid_get(&self.grid, [y, x]).unwrap().set(ROUND);
+				}
+			}
+		}
+	}
+
+	fn sum(&mut self) -> usize {
+		let height = self.grid.len();
+
+		self.grid
+			.iter()
+			.enumerate()
+			.flat_map(|(y, row)| row.iter().map(move |cell| (y, cell)))
+			.map(|(y, cell)| if cell.get() == ROUND { height - y } else { 0 })
+			.sum_self()
+	}
+}
+
+// fn print_grid(grid: &Grid<Cell<u8>>) {
+// 	for row in grid {
+// 		for cell in row {
+// 			print!("{}", DisplayByte(cell.get()));
+// 		}
+// 		println!();
+// 	}
+// 	println!("-------");
+// }
